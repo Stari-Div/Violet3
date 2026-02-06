d6b9d5c4731fe1c06c644c62aea63a671f47bec2
Stari <168887788+Stari-Div@users.noreply.github.com>
2026-02-06 16:00:04 -0500

    Added package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "Voilet3",
+  "version": "1.0.0",
+  "description": "Violet 3 | Internet Freedom",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "@tomphttp/bare-server-node": "2.0.5",
+    "axios": "^1.8.3",
+    "cors": "^2.8.5",
+    "dotenv": "^16.4.5",
+    "express": "^4.21.2",
+    "jquery": "^3.7.1"
+  }
+}
